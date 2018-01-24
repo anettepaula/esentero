b2bdeca375cbe5abf6c223ebd7c5e19be275ed18
ism06659681 <ism06659681@a04.informatica.escoladeltreball.org>
2018-01-24 12:02:53 +0100

    First division.

--- esentero.py
+++ esentero.py
@@ -1,3 +1,9 @@
 #coding: utf8
-x = raw_input("Pon tu division:")
+divisor = int(raw_input("Pon tu divisor:"))
+dividiendo = int(raw_input("Pon tu dividiendo:"))
+x = dividiendo%divisor
+if x != 0 :
+	print "no entera"
+else :
+	print "entera."
 
